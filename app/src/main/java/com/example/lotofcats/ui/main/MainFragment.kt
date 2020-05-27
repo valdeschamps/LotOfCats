1474a9316807a87bc6db8b70f67655295c9c213a
val <valentin.deschamps38@gmail.com>
2020-05-27 21:42:43 +0200

    add fragment ktx

---
+++ app/src/main/java/com/example/lotofcats/ui/main/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.example.lotofcats.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.lotofcats.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/lotofcats/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lotofcats.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
