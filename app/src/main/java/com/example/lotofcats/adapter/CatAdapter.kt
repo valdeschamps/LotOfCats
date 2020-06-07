362cbedbd961adae6808e390b81e029428d4393c
val <valentin.deschamps38@gmail.com>
2020-06-07 21:36:16 +0200

    add glide and displays image

--- app/src/main/java/com/example/lotofcats/adapter/CatAdapter.kt
+++ app/src/main/java/com/example/lotofcats/adapter/CatAdapter.kt
@@ -4,14 +4,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.lotofcats.R
 import com.example.lotofcats.model.Cat
+import kotlinx.android.synthetic.main.card.view.*
 
 class CatAdapter() : RecyclerView.Adapter<CatAdapter.CatViewHolder>() {
     inner class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private var cat = Cat()
         fun displayData(newCat: Cat) {
             cat = newCat
+            Glide.with(itemView).load(cat.url).into(itemView.imageView)
         }
     }
 
