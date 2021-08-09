3c052ae9b24a60194fb5f365dea64c05ccd019b7
valdeschamps <valentin.deschamps38@gmail.com>
2021-08-09 21:13:02 +0200

    prevent click before resource is loaded

--- app/src/main/java/com/example/lotofcats/adapter/CatAdapter.kt
+++ app/src/main/java/com/example/lotofcats/adapter/CatAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.lotofcats.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.example.lotofcats.R
 import com.example.lotofcats.databinding.CardBinding
 import com.example.lotofcats.model.Cat
@@ -17,21 +22,45 @@ import com.example.lotofcats.views.MainFragmentDirections
 
 class CatAdapter : RecyclerView.Adapter<CatAdapter.CatViewHolder>() {
     inner class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val binding = CardBinding.bind(itemView)
+        private val binding = CardBinding.bind(itemView)
+        private var isLoaded = false
         fun displayData(newCat: Cat) {
             ViewCompat.setTransitionName(binding.imageView, newCat.url)
 
-            binding.imageView.setOnClickListener { view ->
-                val url = newCat.url
-                val extra = FragmentNavigatorExtras(
-                    binding.imageView to url
-                )
-                val action = MainFragmentDirections.actionMainFragmentToImageFragment(url)
-                view.findNavController().navigate(action, extra)
-            }
-
             val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
-            Glide.with(itemView).load(newCat.url).placeholder(R.drawable.ic_circle_foreground).apply(requestOptions).into(binding.imageView)
+            Glide.with(itemView)
+                .load(newCat.url)
+                .listener(object :RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        isLoaded = true
+                        return false
+                    }
+
+                })
+                .placeholder(R.drawable.ic_circle_foreground)
+                .apply(requestOptions)
+                .into(binding.imageView)
+
+            binding.imageView.setOnClickListener { view ->
+                if(isLoaded) {
+                    val url = newCat.url
+                    val extra = FragmentNavigatorExtras(
+                        binding.imageView to url
+                    )
+                    val action = MainFragmentDirections.actionMainFragmentToImageFragment(url)
+                    view.findNavController().navigate(action, extra)
+                }
+            }
         }
     }
 
